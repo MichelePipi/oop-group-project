24595dac777e77b3a797bbfe30b604770faee046
Michele Antonio Pipicelli <micheleantonio@Micheles-MacBook-Pro.local>
2025-05-08 12:24:22 +0930

    Start UpgradesManager

---
+++ src/Upgrades/UpgradesManager.cpp
@@ -0,0 +1,12 @@
+#include "UpgradesManager.h"
+#include "Upgrade.h"
+#include <iostream>
+
+UpgradesManager::UpgradesManager() {
+    // implement genericupgrades
+    
+}
+
+UpgradesManager::UpgradesManager(Upgrade** u) {
+    upgrades = u; 
+}
---
+++ src/Upgrades/UpgradesManager.h
@@ -0,0 +1,16 @@
+#ifndef UPGRADES_MANAGER_H
+#define UPGRADES_MANAGER_H
+
+#include "Upgrade.h"
+
+class UpgradesManager {
+    private:
+        Upgrade** upgrades;
+        int cps;
+    public:
+        UpgradesManager(); 
+        UpgradesManager(Upgrade** upgrades);
+        int calculateCurrentCps();
+};
+
+#endif 
